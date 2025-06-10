4f7dab2bc1e0d2652e3aa8944709ad6eff3a6735
Bss <mx.bss@proton.me>
2025-06-10 02:06:33 +0200

    fix cursor style

--- zsh/.config/zsh/plugins.zsh
+++ zsh/.config/zsh/plugins.zsh
@@ -8,6 +8,10 @@
 
 ZVM_INIT_MODE=sourcing # Fix fzf-vi-mode overwrites 
 source $(brew --prefix)/opt/zsh-vi-mode/share/zsh-vi-mode/zsh-vi-mode.plugin.zsh
+ZVM_INSERT_MODE_CURSOR=$ZVM_CURSOR_BLINKING_BLOCK
+ZVM_NORMAL_MODE_CURSOR=$ZVM_CURSOR_BLOCK
+ZVM_OPPEND_MODE_CURSOR=$ZVM_CURSOR_UNDERLINE
+ZVM_VI_HIGHLIGHT_BACKGROUND=#754545
 
 source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
