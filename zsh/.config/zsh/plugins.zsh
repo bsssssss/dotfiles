a7a80aa88ae7f19e17b9da2b28421aeec004999b
Bss <mx.bss@proton.me>
2025-06-10 23:38:34 +0200

    add zsh-vi-mode config function

--- zsh/.config/zsh/plugins.zsh
+++ zsh/.config/zsh/plugins.zsh
@@ -6,15 +6,17 @@
 #   zsh-autosuggestions
 # )
 
-ZVM_INIT_MODE=sourcing # Fix fzf-vi-mode overwrites 
+function zvm_config() {
+    ZVM_INIT_MODE=sourcing # Fix fzf-vi-mode overwrites 
+    ZVM_INSERT_MODE_CURSOR=$ZVM_CURSOR_BLINKING_BLOCK
+    ZVM_NORMAL_MODE_CURSOR=$ZVM_CURSOR_BLOCK
+    ZVM_OPPEND_MODE_CURSOR=$ZVM_CURSOR_UNDERLINE
+    ZVM_VI_HIGHLIGHT_BACKGROUND=#754545
+}
+
 source $(brew --prefix)/opt/zsh-vi-mode/share/zsh-vi-mode/zsh-vi-mode.plugin.zsh
-ZVM_INSERT_MODE_CURSOR=$ZVM_CURSOR_BLINKING_BLOCK
-ZVM_NORMAL_MODE_CURSOR=$ZVM_CURSOR_BLOCK
-ZVM_OPPEND_MODE_CURSOR=$ZVM_CURSOR_UNDERLINE
-ZVM_VI_HIGHLIGHT_BACKGROUND=#754545
 
 source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
 # source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 # bindkey '^@' forward-word # Accept auto-suggestion by word with ctrl-space
-
